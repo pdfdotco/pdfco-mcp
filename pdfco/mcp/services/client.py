b5a2b408fc555f44f6cb7517a2edc14b5aba44cf
ingeol.song <ingeol.song@epapyrus.com>
2025-06-05 11:08:24 +0900

    fix: set error message when X_API_KEY is not configured

--- pdfco/mcp/services/client.py
+++ pdfco/mcp/services/client.py
@@ -6,14 +6,36 @@ import importlib.metadata
 
 __BASE_URL = "https://api.pdf.co"
 X_API_KEY = os.getenv("X_API_KEY")  
-if not X_API_KEY:
-    raise ValueError("Please set X_API_KEY in the environment variables. To get the API key please sign up at https://pdf.co and you can get the API key from the dashboard.")
 
 __version__ = importlib.metadata.version('pdfco-mcp')
 print(f"pdfco-mcp version: {__version__}", file=sys.stderr)
 
 @asynccontextmanager
 async def PDFCoClient() -> AsyncGenerator[AsyncClient, None]:
+    if not X_API_KEY:
+        raise ValueError("""X_API_KEY is not set. Please set X_API_KEY in the environment variables in your MCP server. 
+        To get the API key please sign up at https://pdf.co and you can get the API key from the dashboard.
+        ex) .cursor/mcp.json
+        ```json
+        {
+            "mcpServers": {
+                "pdfco": {
+                    "command": "uv",
+                    "args": [
+                        "--directory",
+                        "/Users/outstandingboy/workspace/pdfco-mcp",
+                        "run",
+                        "pdfco-mcp"
+                    ],
+                    "env": {
+                        "X_API_KEY": "YOUR_API_KEY"
+                    }
+                }
+            }
+        }
+        ```
+        """)
+    
     client = AsyncClient(
             base_url=__BASE_URL,
             headers={
